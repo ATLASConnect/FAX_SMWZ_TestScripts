b1581ac543cdc0cac66e7987102fd5a4a8bb886d
luehring <luehring@indiana.edu>
2015-01-27 15:46:40 -0500

    Add options that run jobs in the background instead submitting them to a batch system (currently condor).

--- SMWZd3pdExample_run.sh
+++ SMWZd3pdExample_run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # $1 Input file type
-# $2 1 local condor, local files, 2 local condor, FAX files, 3 condor flocking via Bosco, 4 ATLAS Connect
+# $2 1 local condor, local files, 2 local condor, FAX files, 3 condor flocking via Bosco,
+#    4 ATLAS Connect, 5 background jobs, local files, 6 background jobs, FAX files
 # $3 Start time of job submission script
 # $4 ROOT version
-# $5 XRootD version
-# $6 X509_USER_PATH
-# $7 RUCIO_ACCOUNT
+# $5 XRootD version (needed for FAX data access)
+# $6 X509_USER_PATH (needed for FAX data access and condor flocking)
+# $7 RUCIO_ACCOUNT (needed for fax data access)
 
 # Setup to get run time of this script.
 start=`date +%s`
@@ -47,21 +48,27 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 # Setup Atlas Local Root Base
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
-# Get task Task (job within jobset)  and Job ID (jobset) numbers.
+# Get task id (job number within jobset)
 # NB: I take the convention that the tasks are numbered 1 to N and not 0 to N-1.
-ProcId=`grep ProcId ${TMP}/.job.ad | cut -d' ' -f3`
+if [[ -z $ProcId ]]; then
+  ProcId=`grep ProcId ${TMP}/.job.ad | cut -d' ' -f3`
+fi
 let "task_id = ProcId + 1"
 # Pad task id with leading zeros to use in file and directory names.
 padded_task_id=`printf %04u ${task_id}`
-ClusterId=`grep -v AutoClusterId ${TMP}/.job.ad | grep ClusterId | cut -d' ' -f3`
-job_id=${ClusterId}
 
-# Save location of working direct for Input/Output files.
-# NB: For some workflows the directory tree where the program runs is not under work_dir.
-work_dir=${PWD}
+# Get jobset id (number used to identify the jobset)
+if [[ -z ${ClusterId} ]]; then
+  ClusterId=`grep -v AutoClusterId ${TMP}/.job.ad | grep ClusterId | cut -d' ' -f3`
+fi
+jobset_id=${ClusterId}
+
+# Save location of working directory for jobset input/output files.
+# NB: For some workflows the directory tree where the program runs is not under jobset_dir.
+jobset_dir=${PWD}
 
 case "${2}" in
-  1)# local condor, local files
+  1) # local condor, local files
     # Setup for Root
     if [[ ${4} != "current-SL6" ]]; then
       localSetupROOT ${4} --skipConfirm
@@ -71,7 +78,7 @@ case "${2}" in
     mkdir ${padded_task_id}/
     cd ${padded_task_id}/
     ;;
-  2)# local condor, FAX data access
+  2) # local condor, FAX data access
     # Setup FAX (Sets up EMI, FAX, RUCIO, ROOT, XRootD)
     export X509_USER_PROXY=${PWD}/${6}
     export RUCIO_ACCOUNT=${7}
@@ -81,7 +88,7 @@ case "${2}" in
     mkdir ${padded_task_id}/
     cd ${padded_task_id}/
     ;;
-  3)# condor flocking, FAX data access
+  3) # condor flocking, FAX data access
     # Setup FAX (Sets up EMI, FAX, RUCIO, ROOT, XRootD)
     export X509_USER_PROXY=${PWD}/${6}
     export RUCIO_ACCOUNT=${7}
@@ -89,7 +96,7 @@ case "${2}" in
     # Confirm the grid proxy is available.
     voms-proxy-info --all
     ;;
-  4)# ATLAS connect, FAX data access
+  4) # ATLAS connect, FAX data access
     # Setup FAX (Sets up EMI, FAX, RUCIO, ROOT, XRootD)
     export X509_USER_PROXY=${PWD}/${6}
     export RUCIO_ACCOUNT=${7}
@@ -97,7 +104,27 @@ case "${2}" in
     # Confirm the grid proxy is available.
     voms-proxy-info --all
     ;;
-  *)# Oopsie!
+  5) # background jobs, local files
+    # Setup Root
+    if [[ ${4} != "current-SL6" ]]; then
+      localSetupROOT ${4} --skipConfirm
+    else
+      localSetupROOT --skipConfirm
+    fi
+    mkdir ${padded_task_id}/
+    cd ${padded_task_id}/
+    ;;
+  6) # background jobs, FAX data access
+    # Setup FAX (Sets up EMI, FAX, RUCIO, ROOT, XRootD)
+    export X509_USER_PROXY=${PWD}/${6}
+    export RUCIO_ACCOUNT=${7}
+    localSetupFAX --rootVersion=${4} --xrootdVersion=${5} --skipConfirm
+    # Confirm the grid proxy is available.
+    voms-proxy-info --all
+    mkdir ${padded_task_id}/
+    cd ${padded_task_id}/
+    ;;
+  *) # oopsie!
     echo "Reached unreachable case."
     return 7
     ;;
@@ -108,17 +135,19 @@ let "first_line = ( ${task_id} - 1)  * 10 + 1"
 let "last_line = first_line + 9"
 input_file=input_${1}.txt
 input_seq_file=input_${padded_task_id}.txt
-output_seq_file=SMWZd3pdExample_${1}_${job_id}_${padded_task_id}.root
+output_seq_file=SMWZd3pdExample_${1}_${jobset_id}_${padded_task_id}.root
 
 # Unpack example code directory tree.
-tar xzf ${work_dir}/SMWZd3pdExample.lib.tgz
+tar xzf ${jobset_dir}/SMWZd3pdExample.lib.tgz
 
-sed -n ${first_line},${last_line}p ${work_dir}/${input_file} > ${work_dir}/${input_seq_file}
-/bin/cat ${work_dir}/${input_seq_file}
-cd ${work_dir}/run
-../bin/SMWZd3pdExample -f ${work_dir}/${input_seq_file} -o ${work_dir}/${output_seq_file}
+sed -n ${first_line},${last_line}p ${jobset_dir}/${input_file} > ${jobset_dir}/${input_seq_file}
+/bin/cat ${jobset_dir}/${input_seq_file}
+# Must be in run directory to avoid trivial warning message killing job with certain
+# recent versions of ROOT and GCC.
+cd run
+../bin/SMWZd3pdExample -f ${jobset_dir}/${input_seq_file} -o ${jobset_dir}/${output_seq_file}
 cd ..
-rm -f ${work_dir}/${input_seq_file}
+rm -f ${jobset_dir}/${input_seq_file}
 
 # Show final set of files.
 echo
@@ -126,19 +155,27 @@ ls -al
 
 # Clean up
 case "${2}" in
-  1)# local condor, local files
+  1) # local condor, local files
     cd ..
     rm -rf ${padded_task_id}/
     ;;
-  2)# local condor, FAX data access
+  2) # local condor, FAX data access
     cd ..
     rm -rf ${padded_task_id}/
     ;;
-  3)# condor flocking, FAX data access
+  3) # condor flocking, FAX data access
     ;;
-  4)# ATLAS connect, FAX data access
+  4) # ATLAS connect, FAX data access
     ;;
-  *)# Oopsie!
+  5) # background jobs, local files
+    cd ..
+    rm -rf ${padded_task_id}/
+    ;;
+  6) # background jobs, FAX data access
+    cd ..
+    rm -rf ${padded_task_id}/
+    ;;
+  *) # Oopsie!
     echo "Reached unreachable case."
     return 8
 esac
--- submit_jobset.sh
+++ submit_jobset.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
+# submit_jobset.sh Submits FAX, condor flocking, and ATLAS connect stress testing jobs.
 # -w Working Directory (default .)
 # -n Number of Jobs (default 1)
 # -i Input File Type (default smwz_p1328_p1329_rcc_FAX))
-# -j Job Type:  1 local batch using condor, local files, 2 local batch using condor, FAX files, 3 condor flocking using Bosco, 4 ATLAS Connect (default 3)
+# -j Job Type:  1 local batch using condor, local files, 2 local batch using condor, FAX files, 3 condor flocking using Bosco,
+#               4 ATLAS Connect, 5 local background jobs, local files, 6 local background jobs, FAX files (default 3)
 # -r ROOT version (default current-SL6)
-# -x XRootD Version (default current-SL6)
-# -X X509_USER_PROXY (defualt /tmp/x509up_u<uid>)
-# -R RUCIO_ACCOUNT (default $USER)
+# -x XRootD Version needed for FAX data access (default current-SL6)
+# -X X509_USER_PROXY needed for FAX data access and condor flocking (defualt /tmp/x509up_u<uid>)
+# -R RUCIO_ACCOUNT needed for FAX data accecc (default $USER)
 function HelpSubmitJobset() { echo "source submit_jobset.sh -w working_directory -n number_of_jobs -i input_file_type -j job_type -r root_version -x xrootd_version -X X509_USER_PROXY -R RUCIO_ACCOUNT -h Print this help message" ; }
 
 start=`date +%s`
@@ -16,16 +18,9 @@ number_of_jobs=1
 input_file_type=smwz_p1328_p1329_rcc_FAX
 job_type=3
 root_version="current-SL6"
-xrootd_version="current-SL6"
-if [[ -z "$X509_USER_PROXY" ]]; then  
-  export X509_USER_PROXY=/tmp/x509up_u`grep ${USER} /etc/passwd | cut -d":" -f3`
-fi
-if [[ -z "$RUCIO_ACCOUNT" ]]; then
-  export RUCIO_ACCOUNT=${USER}
-fi
+xrootd_verstion="current_sl6"
 
-while [ "$#" -gt "0" ]
-do
+while [ "$#" -gt "0" ]; do
   case ${1} in
     -w)
       shift
@@ -74,12 +69,13 @@ done
 
 echo ${working_directory} ${number_of_jobs} ${input_file_type} ${job_type} ${root_version} ${xrootd_version} ${X509_USER_PROXY} ${RUCIO_ACCOUNT}
 
-if [[ ${job_type} -lt 1 || ${job_type} -gt 4 ]]; then
-  echo "Job type code out of range - should be 1-3"
+if [[ ${job_type} -lt 1 || ${job_type} -gt 6 ]]; then
+  echo "Job type code out of range - should be 1-6"
   return 5
 fi
 
-if [[ ${job_type} -ge 2 && ${job_type} -le 4 ]]; then
+if [[ ${job_type} -eq 2 || ${job_type} -eq 3 ||  ${job_type} -eq 4 || ${job_type} -eq 6 ]]; then
+
   if [[ -e ${X509_USER_PROXY} ]]; then
     proxycreatetime=`date -r ${X509_USER_PROXY} +%s`
     let "elapsed = start - proxycreatetime"
@@ -94,49 +90,73 @@ if [[ ${job_type} -ge 2 && ${job_type} -le 4 ]]; then
 fi
 
 if [[ -e ${working_directory} ]]; then
-  echo "Directory exists."
+  echo "Working directory exists and will not be overwritten."
   return 3
 fi
 
-input_file=input_${input_file_type}.txt
-if [[ ! -e ${input_file} ]]; then
-  echo "Input file missing."
+input_data_file_list=input_${input_file_type}.txt
+if [[ ! -e ${input_data_file_list} ]]; then
+  echo "Text file containing list of input data files missing."
   return 4
 fi
 
+if [[ ${job_type} -ge 1 && ${job_type} -le 4 ]]; then # condor submission
+  export Number_of_Jobs=${number_of_jobs}
+  export Input_File_Type=${input_file_type}
+  export Job_Type=${job_type}
+  export ROOT_Version=${root_version}
+  export XRootD_Version=${xrootd_version}
+  export Start=${start}
+else # put jobs in background
+  export ClusterId=${start}
+fi
+
 mkdir ${working_directory}/
-cp ${input_file} ${working_directory}/
+cp ${input_data_file_list} ${working_directory}/
 cp SMWZd3pdExample_run.sh ${working_directory}/ 
 cp SMWZd3pdExample.lib.tgz ${working_directory}/ 
 cd ${working_directory}/
-export Number_of_Jobs=${number_of_jobs}
-export Input_File_Type=${input_file_type}
-export Job_Type=${job_type}
-export ROOT_Version=${root_version}
-export XRootD_Version=${xrootd_version}
-export Start=${start}
 
 case "${job_type}" in
-  1)
+  1) # local condor, local files
     cp ../SMWZd3pdExample_local.jdl .
     condor_submit -disable SMWZd3pdExample_local.jdl
     ;;
-  2)
+  2) # local condor, FAX files
     cp ${X509_USER_PROXY} ./x509_Proxy
     cp ../SMWZd3pdExample_local_FAX.jdl .
     condor_submit -disable SMWZd3pdExample_local_FAX.jdl
     ;;
-  3)
+  3) # condor flocking, FAX files
     cp ${X509_USER_PROXY} ./x509_Proxy
     cp ../SMWZd3pdExample_bosco.jdl .
     condor_submit -disable SMWZd3pdExample_bosco.jdl
     ;;
-  4)
+  4) # ATLAS Connect, FAX files
     cp ${X509_USER_PROXY} ./x509_Proxy
     cp ../SMWZd3pdExample_connect.jdl .
     condor_submit -disable SMWZd3pdExample_connect.jdl
     ;;
-  *)
+  5) # background jobs, local files
+    i=0
+    while [ ${i} -lt ${number_of_jobs} ]; do
+      export ProcId=${i}
+      let "TaskId = i + 1"
+      source SMWZd3pdExample_run.sh ${input_file_type} ${job_type} ${start} ${root_version} dummy0 dummy1 dummy2 >& SMWZd3pdExample_${input_file_type}_${ClusterId}_"`printf %04u ${TaskId}`".log &
+      let "i = i + 1"
+    done
+    ;;
+  6) # background jobs, FAX files
+    cp ${X509_USER_PROXY} ./x509_Proxy
+    i=0
+    while [ ${i} -lt ${number_of_jobs} ]; do
+      export ProcId=${i}
+      let "TaskId = i + 1"
+      source SMWZd3pdExample_run.sh ${input_file_type} ${job_type} ${start} ${root_version} ${xrootd_version} x509_Proxy ${RUCIO_ACCOUNT} >& SMWZd3pdExample_${input_file_type}_${ClusterId}_"`printf %04u ${TaskId}`".log &
+      let "i = i + 1"
+    done
+    ;;
+  *) # oopsie
     echo "Reached unreachable case."
     return 6
     ;;
